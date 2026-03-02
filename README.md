c16c5882250dc3003737c2bcf3fe40f4c3f8035d
serguc <42298249+Serguc@users.noreply.github.com>
2026-03-02 16:36:56 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,57 @@
+# Android device tree for Tablet Blackview MEGA 3
+
+## Device specifications
+
+Basic   | Spec Sheet
+-------:|:-------------------------
+SoC     | Mediatek Helio G100 (MT6789) (6nm)
+CPU     | Octa-core (2x2.2 GHz Cortex-A76 & 6x2.0 GHz Cortex-A55)
+GPU     | Mali-G57 MC2
+Memory  | 12 GB RAM
+Shipped Android Version | Doke OS 4.1 (Android 15)
+Storage | 256 GB
+Battery | Li-Po 8800 mAh, non-removable
+Display | 12.1-inch,1600х2560 FHD+ 2.4K Resolution; Dynamic 120Hz Refresh Rate
+Camera  | 50.1 MP (Samsung® ISOCELL GW3) and 13 MP (Samsung® ISOCELL 3L6)
+Demensions | 278.5x180.5x7.4 mm
+Weight  | 555 g
+
+### Device picture
+
+https://www.devicespecifications.com/en/model/103563ce
+
+## Features
+
+# Building
+Works:
+- [X] ADB
+- [X] Decryption
+- [X] Display
+- [X] Fasbootd
+- [X] Flashing
+- [X] MTP
+- [X] Sideload
+- [X] USB OTG
+- [X] SDCard
+- [ ] Touch
+- [X] Flashlight
+- [ ] Vibrator
+
+## Building
+```bash
+source build/envsetup.sh
+lunch twrp_MEGA_3-eng
+fox_14.1 and twrp_14.1
+lunch twrp_MEGA_3-ap2a-eng
+mka vendorbootimage
+```
+
+## To use it:
+
+```
+fastboot flash vendor_boot vendor_boot.img
+
+```
+### Thanks to
+
+@Serguc
---
+++ device.mk
@@ -0,0 +1,108 @@
+# Copyright (C) 2024 The Android Open Source Project
+# SPDX-License-Identifier: Apache-2.0
+
+# Inherit from common AOSP config
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+DEVICE_PATH := device/Blackview/MEGA_3
+
+# Dynamic
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 32
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 32
+
+# Virtual_ab_ota
+AB_OTA_UPDATER := true
+ENABLE_VIRTUAL_AB := true
+#TARGET_ENFORCE_AB_OTA_PARTITION_LIST := true
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch.mk)
+
+# A/B
+AB_OTA_PARTITIONS += \
+    vbmeta_vendor \
+    boot \
+    vbmeta_system \
+    odm_dlkm \
+    system \
+    system_ext \
+    vendor \
+    vendor_dlkm \
+    product
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/mtk_plpath_utils \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-service \
+    android.hardware.boot@1.2-mtkimpl \
+    android.hardware.boot@1.2-mtkimpl.recovery
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctl
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+# Health Hal
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh
+
+# Update engine
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+# Mtk plpath utils
+PRODUCT_PACKAGES += \
+    mtk_plpath_utils \
+    mtk_plpath_utils.recovery \
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
+# Additional binaries & libraries needed for recovery
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libion \
+    libhidlbase \
+    libxml2 \
+    libpuresoftkeymasterdevice
+
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libhidlbase.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
+
+# snapuserd
+PRODUCT_PACKAGES += snapuserd
+RECOVERY_BINARY_SOURCE_FILES += $(TARGET_OUT_EXECUTABLES)/snapuserd
+
+# Vendor ramdisk
+PRODUCT_COPY_FILES += \
+    device/Blackview/MEGA_3/fstab.emmc:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.emmc \
+    device/Blackview/MEGA_3/fstab.mt6789:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.mt6789
+
+# OEM otacerts
+PRODUCT_EXTRA_RECOVERY_KEYS += \
+    $(DEVICE_PATH)/security/releasekey
