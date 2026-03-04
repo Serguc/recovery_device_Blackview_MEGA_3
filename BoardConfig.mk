c7a0c31c06111d90ad693cd7da7d9c7974708373
serguc <42298249+Serguc@users.noreply.github.com>
2026-03-04 12:59:18 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -130,16 +130,24 @@ BOARD_VNDK_VERSION := current
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
-# Vendor_Boot RECOVERY
+# RECOVERY
 BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_NO_RECOVERY := true
 BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_SUPPRESS_SECURE_ERASE := true
 BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE :=
 BOARD_USES_GENERIC_KERNEL_IMAGE := true
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
+
+# Включаем сборку рамдиска рекавери прямо в vendor_boot
 BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
 BOARD_INCLUDE_RECOVERY_RAMDISK_IN_VENDOR_BOOT := true
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
+
+# Указываем, что у нас нет отдельного раздела recovery
+BOARD_USES_RECOVERY_AS_BOOT := false
+
+# Для корректной упаковки OrangeFox
+TARGET_COPY_OUT_VENDOR_RAMDISK := vendor_boot
 
 # Crypto
 TW_INCLUDE_CRYPTO := false
