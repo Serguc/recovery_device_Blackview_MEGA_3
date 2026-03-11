8473bd679a80bb0a2e8604f94cd52e54434f52c8
serguc <42298249+Serguc@users.noreply.github.com>
2026-03-11 23:45:24 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -47,10 +47,10 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
     export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
 	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
 
-	#export FOX_DELETE_MAGISK_ADDON=1
-    #export FOX_DELETE_AROMAFM=1
-    #export FOX_ENABLE_APP_MANAGER=1
-    #export FOX_SETTINGS_ROOT_DIRECTORY=/persist/OFRP
+    export FOX_DELETE_MAGISK_ADDON=1
+    export FOX_DELETE_AROMAFM=1
+    export FOX_ENABLE_APP_MANAGER=1
+    export FOX_SETTINGS_ROOT_DIRECTORY=/persist/OFRP
     export FOX_RESET_SETTINGS=1
     export FOX_ENABLE_DEBUGGING=1
 
@@ -66,7 +66,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
     #OTA
 	export FOX_AB_DEVICE=1
     export FOX_VIRTUAL_AB_DEVICE=1
-    #export OF_SUPPORT_VBMETA_AVB2_PATCHING=1
+    export OF_SUPPORT_VBMETA_AVB2_PATCHING=1
 
     #Flashlight
     export OF_FL_PATH1=/sys/class/leds/flashlight
